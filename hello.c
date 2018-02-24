3dfd2936164c238b5b5905618edac099f194a8c2
Mayank Sharma <msharma@iitk.ac.in>
2018-02-24 11:41:22 +0530

    User 2- 1st commit

--- hello.c
+++ hello.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+void microkernel_msg(char *);
 int main(){
 	printf("Hello world!\n");
+	microkernel_msg("is more portable");
 	return 0;
+}
+void microkernel_msg(char * a){
+	printf("microkernel : %s\n", a);
 }
